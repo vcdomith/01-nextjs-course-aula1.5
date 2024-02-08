633f3e85ff55ceb27142bb4224c9066dc03ce9e9
Vitor Domith <vcdomith@hotmail.com>
2024-02-08 11:00:36 -0300

    add_faq_style

--- pages/faq.js
+++ pages/faq.js
@@ -1,46 +1,24 @@
-import Head from 'next/head'
-import { useEffect, useState } from 'react';
-import Link from '../src/components/Link';
+// faq.js
+import FAQScreen from '../src/screens/FAQScreen';
 
-// Conteúdo que deve ser carregado ao ser iniciado
+export default FAQScreen; 
+
 export async function getStaticProps() {
-
-    // É apenas rodado no build, ou seja, rodado uma vez quando for criado 
-    const url = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
-
-    const faq = await fetch(url)
-        .then((res) => res.json())
-        .then((res) => res)
-    // const faq = await acesso.json()
+    console.log('Em modo DEV, sempre roda! A cada acesso')
+    console.log('Roda SOMENTE em build time')
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    const faq = await fetch(FAQ_API_URL)
+        .then((respostaDoServidor) => {
+            return respostaDoServidor.json();
+        })
+        .then((resposta) => {
+            return resposta;
+        });
 
     return {
-        props: {
-            faq,
-        },
+      props: {
+          qualquercoisa: 'que eu passar aqui',
+          faq,
+      },
     };
-
-}
-// export async function getServerSideProps()
-// Já o getServerSideProps roda a cada acesso que se tem na página, gerando os elementos da mesma forma que o getStaticProps, porém em cada acesso
-
-export default function FAQPage({ faq }) {
-
-    return (
-        <div>
-            <Head>
-                <title>FAQ - Alura Cases Campanha</title>
-            </Head>
-            <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
-            <Link href="/">
-                Ir para a home
-            </Link>
-            <ul>
-                {faq.map(({answer, question}) => 
-                <li key={question}>
-                    <h2>{question}</h2>
-                    <p>{answer}</p>
-                </li>)}
-            </ul>
-        </div>
-    )
-}
+}  
